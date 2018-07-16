f2d6d87b561435b193597bc40aee42d007fc9299
Julia Son <juliasson@fb.com>
2018-07-16 14:13:30 -0700

    login and signup logic added

---
+++ app/src/main/java/me/juliasson/unipath/LoginActivity.java
@@ -0,0 +1,76 @@
+package me.juliasson.unipath;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bvLogin;
+    private Button bvSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Log.d("LoginActivity", "Login successful");
+            Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        bvLogin = findViewById(R.id.bvLogin);
+        bvSignup = findViewById(R.id.bvSignUp);
+
+        bvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                logIn(username, password);
+            }
+        });
+
+        bvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void logIn(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("login_activity", "Login successful");
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    final Intent i = new Intent(LoginActivity.this, TimelineActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Log.e("login_activity","Login failure");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/me/juliasson/unipath/ProfileActivity.java
+++ app/src/main/java/me/juliasson/unipath/ProfileActivity.java
@@ -1,13 +1,61 @@
 package me.juliasson.unipath;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView tvProgressLabel;
+    private ProgressBar pbProgress;
+    private ImageView ivProfileImage;
+    private TextView tvFirstName;
+    private TextView tvLastName;
+    private TextView tvEmail;
+    private ImageButton bvBack;
+    private Button bvLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvProgressLabel = findViewById(R.id.tvProgressLabel);
+        pbProgress = findViewById(R.id.pbProgress);
+        ivProfileImage = findViewById(R.id.ivProfileImage);
+        tvFirstName = findViewById(R.id.tvFirstName);
+        tvLastName = findViewById(R.id.tvLastName);
+        tvEmail = findViewById(R.id.tvEmail);
+        bvBack = findViewById(R.id.bvBack);
+        bvLogout = findViewById(R.id.bvLogout);
+
+        bvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        bvLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Log.d("ProfileActivity", "Logged out successfully");
+                Toast.makeText(ProfileActivity.this, "Logout successful", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
