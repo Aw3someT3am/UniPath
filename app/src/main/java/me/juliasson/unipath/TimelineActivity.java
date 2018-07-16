8a63751b334720111e1c979ff5777c080ae12c81
Katherine Lazar <katherinelazar@fb.com>
2018-07-16 13:37:32 -0700

    added a bunch more dependencies

--- app/src/main/java/me/juliasson/unipath/TimelineActivity.java
+++ app/src/main/java/me/juliasson/unipath/TimelineActivity.java
@@ -1,13 +1,19 @@
 package me.juliasson.unipath;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 
 public class TimelineActivity extends AppCompatActivity {
+
+    // A reference to our bottom navigation view.
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
     }
+
+
 }
