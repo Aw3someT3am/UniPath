2f7f18bf4578a19882b81c8ca55a627fbf82e1f8
Julia Son <juliasson@fb.com>
2018-07-17 16:46:42 -0700

    continued workings on timeline implementation

--- app/src/main/java/me/juliasson/unipath/LinearTimelineFragment.java
+++ app/src/main/java/me/juliasson/unipath/LinearTimelineFragment.java
@@ -1,6 +1,8 @@
 package me.juliasson.unipath;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,12 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.juliasson.unipath.Model.OrderStatus;
+import me.juliasson.unipath.Model.TimeLine;
+
 public class LinearTimelineFragment extends Fragment {
 
-    private MyExpandableListAdapter adapter;
-    private LinearLayoutManager layoutManager;
-    private RecyclerView feedRecycler;
-
+    private RecyclerView mRecyclerView;
+    private TimeLineAdapter mTimeLineAdapter;
+    private List<TimeLine> mDataList = new ArrayList<>();
+    private Context mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -24,7 +32,32 @@ public class LinearTimelineFragment extends Fragment {
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        mContext = view.getContext();
 
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setHasFixedSize(true);
+
+        initView();
+    }
+
+    private void initView() {
+        setDataListItems();
+        mTimeLineAdapter = new TimeLineAdapter(mDataList);
+        mRecyclerView.setAdapter(mTimeLineAdapter);
+    }
+
+    private void setDataListItems(){
+        //TODO: REPLACE ITEMS
+        mDataList.add(new TimeLine("Item successfully delivered", "", OrderStatus.INACTIVE));
+        mDataList.add(new TimeLine("Courier is out to delivery your order", "2017-02-12 08:00", OrderStatus.ACTIVE));
+        mDataList.add(new TimeLine("Item has reached courier facility at New Delhi", "2017-02-11 21:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Item has been given to the courier", "2017-02-11 18:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Item is packed and will dispatch soon", "2017-02-11 09:30", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Order is being readied for dispatch", "2017-02-11 08:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Order processing initiated", "2017-02-10 15:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Order confirmed by seller", "2017-02-10 14:30", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLine("Order placed successfully", "2017-02-10 14:00", OrderStatus.COMPLETED));
     }
 }
