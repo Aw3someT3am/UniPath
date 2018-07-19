720ba252ae17aaa58e3abae969777bee5a0e358d
Julia Son <juliasson@fb.com>
2018-07-19 13:12:42 -0700

    implemented adding colleges and deadlines to user relations when like button is clicked

--- app/src/main/java/me/juliasson/unipath/adapters/CollegeAdapter.java
+++ app/src/main/java/me/juliasson/unipath/adapters/CollegeAdapter.java
@@ -3,6 +3,7 @@ package me.juliasson.unipath.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,24 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.like.LikeButton;
+import com.like.OnLikeListener;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import me.juliasson.unipath.R;
 import me.juliasson.unipath.model.College;
+import me.juliasson.unipath.model.CollegeDeadlineRelation;
+import me.juliasson.unipath.model.UserCollegeRelation;
+import me.juliasson.unipath.model.UserDeadlineRelation;
 
 public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.ViewHolder> implements Filterable{
 
@@ -47,15 +58,101 @@ public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CollegeAdapter.ViewHolder viewHolder, int position) {
-        College college = mColleges.get(position);
+    public void onBindViewHolder(@NonNull final CollegeAdapter.ViewHolder viewHolder, int position) {
+        final College college = mColleges.get(position);
         viewHolder.tvCollegeName.setText(college.getString(KEY_COLLEGE_NAME));
 
         Glide.with(mContext)
                 .load(college.getParseFile(KEY_COLLEGE_IMAGE).getUrl())
                 .into(viewHolder.ivCollegeImage);
 
-        //TODO: IMPLEMENT BUTTON CLICK
+        loadFavoriteColleges(viewHolder, college);
+
+        //TODO: IMPLEMENT
+        viewHolder.lbLikeButton.setOnLikeListener(new OnLikeListener() {
+            @Override
+            public void liked(LikeButton likeButton) {
+                addUserCollegeRelation(college);
+                addUserDeadlineRelations(college);
+            }
+
+            @Override
+            public void unLiked(LikeButton likeButton) {
+
+            }
+        });
+    }
+
+    public void addUserCollegeRelation(final College college) {
+        UserCollegeRelation userCollegeRelation = new UserCollegeRelation();
+        userCollegeRelation.setUser(ParseUser.getCurrentUser());
+        userCollegeRelation.setCollege(college);
+        userCollegeRelation.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("CollegeAdapter", "Create UserCollegeRelation success");
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void addUserDeadlineRelations(final College college) {
+        CollegeDeadlineRelation.Query cdQuery = new CollegeDeadlineRelation.Query();
+        cdQuery.getTop().withDeadline().withCollege();
+
+        cdQuery.findInBackground(new FindCallback<CollegeDeadlineRelation>() {
+            @Override
+            public void done(List<CollegeDeadlineRelation> objects, ParseException e) {
+                if (e == null) {
+                    for(int i = 0; i < objects.size(); i++) {
+                        CollegeDeadlineRelation relation = objects.get(i);
+                        if (relation.getCollege().getObjectId().equals(college.getObjectId())) {
+                            UserDeadlineRelation userDeadlineRelation = new UserDeadlineRelation();
+                            userDeadlineRelation.setUser(ParseUser.getCurrentUser());
+                            userDeadlineRelation.setDeadline(relation.getDeadline());
+                            userDeadlineRelation.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    if (e == null) {
+                                        Log.d("CollegeAdapter", "Create UserDeadlineRelation success");
+                                        Toast.makeText(mContext, "College and Deadlines added!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void loadFavoriteColleges(@NonNull final CollegeAdapter.ViewHolder viewHolder, final College college) {
+        UserCollegeRelation.Query ucQuery = new UserCollegeRelation.Query();
+        ucQuery.getTop().withUser().withCollege();
+
+        ucQuery.findInBackground(new FindCallback<UserCollegeRelation>() {
+            @Override
+            public void done(List<UserCollegeRelation> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        UserCollegeRelation relation = objects.get(i);
+                        if(relation.getCollege().getObjectId().equals(college.getObjectId()) &&
+                                relation.getUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
+                            viewHolder.lbLikeButton.setLiked(true);
+                        }
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -67,6 +164,7 @@ public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.ViewHold
 
         public TextView tvCollegeName;
         public ImageView ivCollegeImage;
+        public LikeButton lbLikeButton;
 
 
         public ViewHolder(View itemView) {
@@ -74,6 +172,7 @@ public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.ViewHold
 
             tvCollegeName = itemView.findViewById(R.id.tvCollegeName);
             ivCollegeImage = itemView.findViewById(R.id.ivCollegeImage);
+            lbLikeButton = itemView.findViewById(R.id.lbLikeButton);
 
             itemView.setOnClickListener(this);
         }
--- app/src/main/java/me/juliasson/unipath/fragments/FavoritesFragment.java
+++ app/src/main/java/me/juliasson/unipath/fragments/FavoritesFragment.java
@@ -2,6 +2,7 @@ package me.juliasson.unipath.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -26,6 +27,7 @@ import me.juliasson.unipath.utils.DateTimeUtils;
 
 public class FavoritesFragment extends Fragment {
 
+    private SwipeRefreshLayout swipeContainer;
     CollegeAdapter collegeAdapter;
     ArrayList<College> colleges;
     RecyclerView rvColleges;
@@ -45,6 +47,24 @@ public class FavoritesFragment extends Fragment {
         rvColleges.setAdapter(collegeAdapter);
 
         loadFavoriteColleges();
+
+        //find the swipe container
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                refresh();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     public void loadFavoriteColleges() {
@@ -68,6 +88,14 @@ public class FavoritesFragment extends Fragment {
                 }
             }
         });
+    }
+
+    public void refresh() {
+        collegeAdapter.clear();
+        loadFavoriteColleges();
+        collegeAdapter.addAll(colleges);
+        // Now we call setRefreshing(false) to signal refresh has finished
+        swipeContainer.setRefreshing(false);
     }
 
     //---------------QUERIES----------------
