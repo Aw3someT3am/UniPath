0733725a9f0969d801a80185a606459bc14501f4
Julia Son <juliasson@fb.com>
2018-07-20 14:33:43 -0700

    making sure jorge has a working copy????

--- app/src/main/java/me/juliasson/unipath/activities/DeadlineDetailsActivity.java
+++ app/src/main/java/me/juliasson/unipath/activities/DeadlineDetailsActivity.java
@@ -1,14 +1,7 @@
 package me.juliasson.unipath.activities;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,18 +10,10 @@ import com.bumptech.glide.Glide;
 import org.parceler.Parcels;
 
 import me.juliasson.unipath.R;
-import me.juliasson.unipath.fragments.DeadlineFragment;
-import me.juliasson.unipath.fragments.GeneralInfoFragment;
 import me.juliasson.unipath.model.College;
 
 
 public class DeadlineDetailsActivity extends AppCompatActivity {
-    FrameLayout flContainer;
-    FragmentTransaction fragmentTransaction;
-
-    Fragment generalInfoFragmnet;
-    Fragment deadlinesFragmnet;
-
     TextView tvcollegeName;
     ImageView ivCollegeImage;
 
@@ -53,37 +38,7 @@ public class DeadlineDetailsActivity extends AppCompatActivity {
         Bundle args = new Bundle();
         args.putParcelable("college", college);
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
 
-        fragmentTransaction = fragmentManager.beginTransaction();
-
-        generalInfoFragmnet = new GeneralInfoFragment();
-        deadlinesFragmnet = new DeadlineFragment();
-
-        generalInfoFragmnet.setArguments(args);
-        deadlinesFragmnet.setArguments(args);
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.flContainer, generalInfoFragmnet).commit();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                fragmentTransaction = fragmentManager.beginTransaction();
-
-                switch (item.getItemId()) {
-                    case R.id.fragment_info:
-                        fragmentTransaction.replace(R.id.flContainer, generalInfoFragmnet).commit();
-                        return true;
-                    case R.id.fragment_deadline:
-                        fragmentTransaction.replace(R.id.flContainer, deadlinesFragmnet).commit();
-                        return true;
-                }
-                return true;
-            }
-        });
     }
 }
 
