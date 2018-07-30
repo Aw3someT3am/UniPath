2b370d3a6952d8d9cbf75d1481104d192ebfdaf9
Julia Son <juliasson@fb.com>
2018-07-30 16:50:14 -0700

    custom deadline can now be added

---
+++ app/src/main/java/me/juliasson/unipath/activities/NDCollegeListDialog.java
@@ -0,0 +1,91 @@
+package me.juliasson.unipath.activities;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Window;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.juliasson.unipath.R;
+import me.juliasson.unipath.adapters.NDCollegeListAdapter;
+import me.juliasson.unipath.model.College;
+import me.juliasson.unipath.model.UserCollegeRelation;
+import me.juliasson.unipath.utils.GetCollegeInterface;
+
+public class NDCollegeListDialog extends AppCompatActivity implements GetCollegeInterface {
+
+    private RecyclerView rvCollegeList;
+    private NDCollegeListAdapter collegeAdapter;
+    private ArrayList<College> colleges;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_nd_college_picker);
+        this.setFinishOnTouchOutside(true);
+
+        setSize();
+
+        rvCollegeList = findViewById(R.id.rvCollegeList);
+        rvCollegeList.setLayoutManager(new LinearLayoutManager(this));
+        colleges = new ArrayList<>();
+        collegeAdapter = new NDCollegeListAdapter(colleges, this);
+        rvCollegeList.setAdapter(collegeAdapter);
+
+        loadFavoriteColleges();
+    }
+
+    public void loadFavoriteColleges() {
+        final UserCollegeRelation.Query ucRelationQuery = new UserCollegeRelation.Query();
+        ucRelationQuery.getTop().withCollege().withUser();
+
+        ucRelationQuery.findInBackground(new FindCallback<UserCollegeRelation>() {
+            @Override
+            public void done(List<UserCollegeRelation> objects, ParseException e) {
+                if (e == null) {
+                    for(int i = 0; i < objects.size(); i++) {
+                        UserCollegeRelation relation = objects.get(i);
+                        if (ParseUser.getCurrentUser().getObjectId().equals(relation.getUser().getObjectId())) {
+                            College college = relation.getCollege();
+                            colleges.add(college);
+                            collegeAdapter.notifyItemInserted(colleges.size()-1);
+                        }
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void setSize() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int height = metrics.heightPixels;
+
+        getWindow().setLayout(ActionBar.LayoutParams.MATCH_PARENT, (5 * height)/6);
+    }
+
+    @Override
+    public void setCollege(College college) {
+        Log.d("NDCollegeListDialog", String.format("%s", college.getCollegeName()));
+        Intent intent = new Intent();
+        intent.putExtra(College.class.getSimpleName(), Parcels.wrap(college));
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/me/juliasson/unipath/adapters/NDCollegeListAdapter.java
@@ -0,0 +1,89 @@
+package me.juliasson.unipath.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.like.LikeButton;
+
+import java.util.ArrayList;
+
+import me.juliasson.unipath.R;
+import me.juliasson.unipath.model.College;
+import me.juliasson.unipath.utils.GetCollegeInterface;
+
+public class NDCollegeListAdapter extends RecyclerView.Adapter<NDCollegeListAdapter.ViewHolder> {
+
+    private ArrayList<College> mColleges;
+    private static Context mContext;
+    private GetCollegeInterface ndGetCollegeInterface;
+
+    private final static String KEY_COLLEGE_IMAGE = "image";
+
+    public NDCollegeListAdapter(ArrayList<College> arrayList, GetCollegeInterface ndInterface) {
+        mColleges = arrayList;
+        ndGetCollegeInterface = ndInterface;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        mContext = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        View collegeView = inflater.inflate(R.layout.card_row, viewGroup, false);
+        NDCollegeListAdapter.ViewHolder viewHolder = new NDCollegeListAdapter.ViewHolder(collegeView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        final College college = mColleges.get(position);
+        viewHolder.tvCollegeName.setText(college.getCollegeName());
+
+        Glide.with(mContext)
+                .load(college.getParseFile(KEY_COLLEGE_IMAGE).getUrl())
+                .into(viewHolder.ivCollegeImage);
+
+        viewHolder.lbLikeButton.setLiked(true);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mColleges.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView tvCollegeName;
+        public ImageView ivCollegeImage;
+        public LikeButton lbLikeButton;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvCollegeName = itemView.findViewById(R.id.tvCollegeName);
+            ivCollegeImage = itemView.findViewById(R.id.ivCollegeImage);
+            lbLikeButton = itemView.findViewById(R.id.lbLikeButton);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+
+            if (position != RecyclerView.NO_POSITION) {
+                College college = mColleges.get(position);
+                ndGetCollegeInterface.setCollege(college);
+            }
+        }
+    }
+}
